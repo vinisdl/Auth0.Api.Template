613747894bd2564ac3a77e10400a7efec86d8b52
vinisdl <vinisdl@hotmail.com>
2019-04-15 19:57:30 -0300

    init commit

---
+++ Auth0ApiTemplate/Controllers/InfoController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Auth0.AuthenticationApi;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Auth0.Api.Template.Controllers
+{
+    [Route("api/{controller}")]
+    public class InfoController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public InfoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("userid")]
+        public object UserId()
+        {
+            // The user's ID is available in the NameIdentifier claim
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return new
+            {
+                UserId = userId
+            };
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("userinfo")]
+        public async Task<object> UserInformation()
+        {
+            // Retrieve the access_token claim which we saved in the OnTokenValidated event
+            var accessToken = User.Claims.FirstOrDefault(c => c.Type == "access_token")?.Value;
+
+            // If we have an access_token, then retrieve the user's information
+            if (string.IsNullOrEmpty(accessToken))
+                return null;
+
+            var apiClient = new AuthenticationApiClient(_configuration["auth0:domain"]);
+            var userInfo = await apiClient.GetUserInfoAsync(accessToken);
+
+            return userInfo;
+
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("claims")]
+        public object Claims()
+        {
+            return User.Claims.Select(c => new
+            {
+                c.Type, c.Value
+            });
+        }
+    }
+}
---
+++ Auth0ApiTemplate/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth0.Api.Template
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            string domain = $"https://{Configuration["Auth0:Domain"]}/";
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;                
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0:Audience"];
+
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        // Grab the raw value of the token, and store it as a claim so we can retrieve it again later in the request pipeline
+                        // Have a look at the ValuesController.UserInformation() method to see how to retrieve it and use it to retrieve the
+                        // user's information from the /userinfo endpoint
+                        if (context.SecurityToken is JwtSecurityToken token)
+                        {
+                            if (context.Principal.Identity is ClaimsIdentity identity)
+                            {
+                                identity.AddClaim(new Claim("access_token", token.RawData));
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                };
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+
+            app.UseAuthentication();
+
+
+            app.UseMvc();
+        }
+    }
+}
